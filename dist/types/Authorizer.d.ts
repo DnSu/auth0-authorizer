7f624dd09adde1459f3e5da80e08518f94c28df9
Daniel Su <daniel@spiralblue.nyc>
2025-02-27 00:28:35 -0500

    fix: export types

--- dist/types/Authorizer.d.ts
+++ dist/types/Authorizer.d.ts
@@ -1,5 +1,5 @@
 import { AuthOConfig, AuthorizerEvent } from "./Authorizer.interface";
 import getAuthInfo from "./utils/getAuthInfo";
 declare const auth0Authorizier: (auth0Config: AuthOConfig, event: AuthorizerEvent, context: unknown, callback: any) => Promise<void>;
-export { getAuthInfo };
+export { getAuthInfo, AuthorizerEvent, AuthOConfig };
 export default auth0Authorizier;
